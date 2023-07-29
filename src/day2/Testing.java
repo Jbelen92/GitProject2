50114b2c18041c4c0a6b92fb721639eaec0805f3
Jefreey A <Jefreey A@DESKTOP-DN6I8K8>
2023-07-29 11:40:02 -0400

    Initial

---
+++ src/day2/Testing.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Testing { 
+	
+	public static void main(String args []) {
+		
+		System.out.println("Jorge");
+	}
+	
+	public static void add( int a, int b) {
+		
+		System.out.println(a+b);
+	}
+
+}
