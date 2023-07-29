0da10fac462c8351b7871ba1c935c9e1fa392916
Jefreey A <Jefreey A@DESKTOP-DN6I8K8>
2023-07-29 11:44:36 -0400

    Initial

--- src/day2/Testing.java
+++ src/day2/Testing.java
@@ -4,6 +4,8 @@ public class Testing {
 	
 	public static void main(String args []) {
 		
+		String country = "USA";
+		
 		System.out.println("Jorge");
 	}
 	
@@ -16,5 +18,7 @@ public class Testing {
 		
 		System.out.println(a+b+c);
 	}
+	
+	
 
 }
